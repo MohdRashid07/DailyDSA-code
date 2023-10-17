1d77d1c8d0a926f7ecd00fdd63c65f341b40caf1
Mohd Rashid <rashidmohd2274@gmail.com>
2023-10-17 09:34:58 -0700

    Program to Convert Kilometers to Miles

---
+++ Convert-Kilometers-To-Miles.js
@@ -0,0 +1,13 @@
+
+// 6. Program to Convert Kilometers to Miles
+const prompt = require('prompt-sync')()
+// taking kilometers input from the user
+const kilometers = prompt("Enter value in kilometers: ")
+
+// conversion factor
+const factor = 0.621371
+
+// calculate miles
+const miles = kilometers * factor
+
+console.log(`${kilometers} kilometers is equal to ${miles} miles.`);
