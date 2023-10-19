3bf389316b9c05f3e32250b7d3e6d7e44a0f6ee2
Mohd Rashid <rashidmohd2274@gmail.com>
2023-10-19 08:41:56 -0700

    Program to find the square root of a number~

---
+++ Sq-root-number.js
@@ -0,0 +1,14 @@
+// 7. Program to find the square root of a number
+
+var num1 = 16;
+var sq1 = Math.sqrt(num1);
+var num2 = 2.25;
+var sq2 = Math.sqrt(num2);
+var num3 = -4;
+var sq3 = Math.sqrt(num3);
+var num4 = 'hello';
+var sq4 = Math.sqrt(num4);
+console.log("The Square Root of the given number is :" , sq1);
+console.log("The Square Root of the given number is :" , sq2);
+console.log("The Square Root of the given number is :" , sq3);
+console.log("The Square Root of the given number is :" , sq4);
